976d415400b841d2b8e7137547d1b88ceb6073b1
Eric Garnett <egcodes@gmail.com>
2018-03-12 11:31:57 -0400

    itsalive section of testingWiki complete

---
+++ test/itsalive.js
@@ -0,0 +1,34 @@
+const expect = require('chai').expect
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+
+describe('Simple math confirmation', () => {
+  it('does really simple addition', () => {
+    expect(2 + 2).to.equal(4)
+  })
+})
+
+
+describe('Actual time of timeout', () => {
+
+  it('tests if a setTimout of 1000 ms really takes approximately that long', (done) => {
+    let startTime = new Date()
+    setTimeout(() => {
+      let actualTime = new Date() - startTime
+      expect(actualTime).to.be.closeTo(1000, 100)
+      done()
+    }, 1000)
+  })
+})
+
+describe('array.forEach', () => {
+  it('Counts invocations of the array forEach method', () => {
+    let arr = [1, 2, 3, 4]
+    let count = 0
+    let func = (num) => {count += num}
+    func = chai.spy(func);
+    arr.forEach(func)
+    expect(func).to.have.been.called.exactly(arr.length)
+  })
+})
